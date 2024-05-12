c111d9d0f64d1458b49be572fb421e76274e5ce4
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-05-12 17:43:08 -0300

    chore: adjust log text

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export const connectMongooseDB = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017')
-    console.log(`✓ Mongoose was coneected with success...`)
+    console.log(`✓ Mongoose was connected with success...`)
   } catch(error) {
     console.error("Error to connect Mongoose: ", error)
   }
