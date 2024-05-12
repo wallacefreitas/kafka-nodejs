1a0e9c5189edd9aae9321a6c4f1ece8ceba646c0
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-05-12 18:11:47 -0300

    chore: adjusts connection event to can't permite empty orders

--- src/events/connection-event.ts
+++ src/events/connection-event.ts
@@ -2,8 +2,10 @@ import { Order } from "../database/schemas";
 import { io } from "../services/socket-io";
 
 export async function connectionEvent() {
-  io.on("connection", (socket) => {
-    const orders = Order.find();
-    socket.emit('consume-orders', JSON.stringify(orders))
+  io.on("connection", async (socket) => {
+    const orders = await Order.find();
+
+    if (orders.length > 0)
+      socket.emit('consume-orders', JSON.stringify(orders))
   });
 }
