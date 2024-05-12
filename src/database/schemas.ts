136dc3aae0d415ceafd21b5f3fa8116afd7bd9fa
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-05-12 17:55:42 -0300

    chore: removed comment

--- src/database/schemas.ts
+++ src/database/schemas.ts
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from 'mongoose';
 
 const Order = mongoose.model('Order', new Schema({
-    code: String, // String is shorthand for {type: String}
+    code: String,
     client: String,
     store: String,
     origin: String,
