811677ffc8de0bf6af5de9e6d6ae638c0e6641f7
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-05-13 22:18:16 -0300

    feat: changed url to environment variable

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
+import env from 'env-var'
 
 export const connectMongooseDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017')
+    await mongoose.connect(`mongodb://${env.get('MONGODB_URL').required().asUrlString()}`)
     console.log(`✓ Mongoose was connected with success...`)
   } catch(error) {
     console.error("Error to connect Mongoose: ", error)
