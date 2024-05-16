67aeca7bc81b8934c3519a85a346d15a7b18178b
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-05-15 21:27:25 -0300

    test: created a test file to simulate a charge

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,39 @@
-import { getOrdersCRM } from './gateway/get-orders'
-import { ApacheKafkaService } from './services/apache-kafka'
+import { ApacheKafkaService } from './services/apache-kafka';
 
-const ordersCRM = getOrdersCRM();
-const apacheKafkaService = new ApacheKafkaService();
+(async function runTest() {
+  const apacheKafkaService = new ApacheKafkaService();
+  const ordersProduce = [
+    apacheKafkaService.produce('topic-orders-crm', createOrdersMockup("crm")),
+    apacheKafkaService.produce('topic-orders-salesforce', createOrdersMockup("salesforce")),
+    apacheKafkaService.produce('topic-orders-senior', createOrdersMockup("senior"))
+  ];
 
-apacheKafkaService.produce('topic-orders-crm', ordersCRM);
+  Promise.all(ordersProduce)
+    .then(() => console.log(`Test executed with success`))
+    .catch((error) => console.log(`Test executed with error: `, error))
+})()
+
+function createOrdersMockup(origin: string) {
+  const ordersMockup = []
+
+  for(let i=0; i < 3; i++) {
+    ordersMockup.push({
+      order: {
+        code: `ERP00${i.toString()}`,
+        client: "000035",
+        store: "0001",
+        origin
+      },
+      items: [
+        {
+          product: `00000${i.toString()}`,
+          description: `Product ${i.toString()}`,
+          quantity: 63,
+          price: 15.79
+        }
+      ]
+    })
+  }
+
+  return ordersMockup
+}
